316e60ef1495c2eca475c8fc43fc157e2ac1783a
Y-5418 <74748919+Y-5418@users.noreply.github.com>
2022-09-06 10:54:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oH0O_6vDEQbYiuKCTkVazKILwQ0w",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "CAviGdzHosfqtlRZTp6W345faj3exil48PTVc_tlfh0",
+      useTemplateId: "K8M2WZow0lqFok76PC6jhL32oLnhJObyuALusN1k9WY",
       // 所在省份
       province: "甘肃",
       // 所在城市
@@ -46,7 +46,7 @@ export const config = {
     {
       name: "杨进秀", 
       id: "",
-      useTemplateId: "CAviGdzHosfqtlRZTp6W345faj3exil48PTVc_tlfh0",
+      useTemplateId: "K8M2WZow0lqFok76PC6jhL32oLnhJObyuALusN1k9WY",
       province: "甘肃",
       city: "兰州",
       openUrl: "https://y-5418.github.io/"
