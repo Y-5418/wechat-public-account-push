690dad78b0a554a6e31eee02b74dd7f97495b5b6
Y-5418 <74748919+Y-5418@users.noreply.github.com>
2022-09-15 09:14:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const config = {
       // 所在城市
       city: "天水",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "http://love.hunlipic.com/web.php?id=RXyLJp7"
+      openUrl: "https://y-5418.github.io/"
     }, 
     {
       name: "杨进秀", 
